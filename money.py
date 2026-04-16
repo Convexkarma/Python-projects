fda6a013a157df7b2afe7c3ef8e6d68c560991c2
Emmanuel Kibet <emmanuelkibet585@gmail.com>
2026-04-16 15:58:19 +0300

    Updated money.py

--- money.py
+++ money.py
@@ -1,21 +1,20 @@
 #!/usr/local/bin/python3
 
-networth = input ('''
+networth = int(input ('''
 What is your networth ?
- A. $0 - $100,000
- B. $100,001 - $500,000
- C. $500,001 - $1,000,000  
+ 1. $0 - $100,000
+ 2. $100,001 - $500,000
+ 3. $500,001 - $1,000,000  
+ Answer Here: '''))
 
-''')
-a = 1
-b = 2
-c = 3
+if networth == 1:
+    print ("\nSorry, you are not eligible for KCB platinum services\n")
 
-if networth.upper() == A:
-    print ("Sorry, you are not eligible for KCB platinum services")
+elif networth == 2:
+    print ("\nCongratulations , you have made the cut, enjoy KCB's platinum services\n")    
 
-elif networth.upper() == B:
-    print ("Congratulations , you have made the cut, enjoy KCB's platinum services")    
+elif networth ==3 :
+    print ("\nSplendid , you are more than eligible for KCB's platinum services ,looking good !!\n")
 
-else :
-    print ("Splendid , you are more than eligible for KCB's platinum services ,looking good !!")
+else:
+    print("\nYou have entered an invalid number\n")
